4a9869df33885b3b32e8c6d34ff2b82e12b339aa
Andrew Shin <andyrpg@gmail.com>
2020-01-13 13:57:10 -0800

    got stack navigator working, will add signup screen to button functionality

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheets } from 'react-native';
 import Signin from './src/screens/Signin';
-import Header from './src/components/Header';
+import Signup from './src/screens/Signup';
+// import Header from './src/components/Header';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import signinReducer from './src/reducers/signinReducer'
+import signinReducer from './src/reducers/signinReducer';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
 
 const store = createStore(signinReducer);
+
+const navigator = createStackNavigator({
+    Signin: Signin,
+    Signup: Signup,
+}, 
+{
+    initialRouteName: 'Signin',
+    defaultNavigationOptions: { 
+        title: 'Passage Panther'
+    }
+}
+);
+
+let AppNavigation = createAppContainer(navigator);
 
 class App extends React.Component {
         render() { 
             return (
-            <View>
                 <Provider store = { store }>
-                    <Header/>
-                    <Signin/>
+                    <AppNavigation />
                 </Provider>
-            </View>
             )
         }
     }
